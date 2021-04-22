71519f2cba239ca454ec3f67d185a91cbe54cfb8
Rutvik <rutvikmakvana001804@gmail.com>
2021-04-22 18:06:16 +0530

    Add Feature Task mark as Complete

--- main.js
+++ main.js
@@ -209,3 +209,18 @@ function deleteTask(dlt) {
     console.log("Task Permanently Deleted From Todo");
     console.log(todo);
 }
+
+// Task Go To Todo to Completed Task
+function completedTask(ele) {
+    var index = document.getElementById(ele.id);
+    index = index.id;
+    var last = index.toString().split('btncompleted', 2);
+    
+    completedTodo.push(todo[last[1]]);
+    todo.splice(last[1],1);
+
+    display(this.todo, this.completedTodo);
+    
+    console.log("Task Go To Completed");
+    console.log(completedTodo);
+}
