18f3f28e57bb96620aaa756fb36afab5564b8d69
Rutvik <rutvikmakvana001804@gmail.com>
2021-04-22 18:12:16 +0530

    Add Feature Task mark as Incomplete

--- main.js
+++ main.js
@@ -238,3 +238,25 @@ function removeTask(dlt) {
     console.log("Task Permanently Deleted From Completed Task");
     console.log(completedTodo);
 }
+
+// Task Go To Completed to Todo Task
+function incompletedTask(ele) {
+    var index = document.getElementById(ele.id);
+    index = index.id;
+    var last = index.toString().split('btnincompleted', 2);
+    
+    todo.push(completedTodo[last[1]]);
+    completedTodo.splice(last[1],1);
+    
+    var a = document.getElementById('toast');    
+    a.className = 'show';
+    setTimeout(() => { 
+        a.className = a.className.replace('show','');
+    }, 3000);
+    a.innerHTML = "Task Back Todo Successfully";
+
+    display(this.todo, this.completedTodo);
+    
+    console.log("Task Back To Go Todo");
+    console.log(completedTodo);
+}
